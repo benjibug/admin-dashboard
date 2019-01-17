70df3473dc2ee3437bd86c5c655f09499327cf04
ben <benjamin.ball24@gmail.com>
2019-01-17 21:58:18 +0000

    two widgets working

--- static/scripts.js
+++ static/scripts.js
@@ -1,42 +1,50 @@
 
-$(document).ready(function() {
+$(document).ready(() => {
     
     var product_one = {
         "name": "shoes",
         "price": 10,
+        
         "discount_price": 10,
-        "currency": "£",
         "quantity": 1
     }
     
-    const post_discount_product_price = document.getElementById("post_discount_product_price");
-    const product_name = document.getElementById("product_name");
-    const product_unit_price = document.getElementById("product_unit_price");
-    const product_discount_slider = document.getElementById("product_unit_slider");
+    const product_name = document.querySelector("#product_name");
+    const product_price = document.querySelector("#product_price");
+    const product_post_discount_price = document.querySelectorAll("#product_post_discount_price");
+   
+    const product_discount_slider = document.querySelectorAll("#product_unit_slider");
 
+    product_name.value = product_one.name
+    product_price.value = product_one.price
 
-    product_discount_slider.addEventListener("input", function(){
+    var get_discounted_price = (percent_to_discount, value, quantity) => {
+        var discount = (100 - percent_to_discount) / 100
+        var unrounded_figure = discount * value * quantity
+        return Number.parseFloat(unrounded_figure).toFixed(2)
+    }
+
+    for (let i=0; i <= product_discount_slider.length; i++){
+
+        product_post_discount_price[i].innerHTML = "£" + product_one.price
+    
+        product_discount_slider[i].addEventListener("input", function () {
+            
+            product_one.discount_price = get_discounted_price(this.value, product_price.value, product_one.quantity)
+            product_post_discount_price[i].innerHTML = "£" + product_one.discount_price
+            
+        });
+
+        product_price.addEventListener("input", function () {
         
-        product_unit_price.value = product_one.price
-        var current_price = product_unit_price.value
-        var discount = (100 - this.value) / 100 
-        var discounted_product_price = discount * current_price * product_one.quantity
-        product_one.discount_price = Number.parseFloat(discounted_product_price).toFixed(2)
-        post_discount_product_price.innerHTML = product_one.currency + product_one.discount_price
+            product_one.price = this.value
+            var parsed_product_price = get_discounted_price(product_discount_slider[i].value, this.value, product_one.quantity)
+            product_post_discount_price[i].innerHTML = "£" + parsed_product_price
+            
+        })
+
+    }
     
-    });
-    
-
-
-    product_unit_price.addEventListener("input", function() {
-
-        var product_discount = (100 - product_discount_slider.value) / 100 
-        var discounted_product_price = product_discount * this.value * product_quantity
-        var parsed_product_price = Number.parseFloat(discounted_product_price).toFixed(2)
-        post_discount_product_price.innerHTML = "£" + parsed_product_price
-
-    })
-
 
     // adds active class to current view on navbar
     var view="#"+$("#view").data().name;
@@ -48,11 +56,11 @@ $(document).ready(function() {
     // var rangeValue = function(){
         
     //     var newValue = this.value+"%";
-    //     var target = document.getElementById("unitDisc"+this.dataset.qty);
+    //     var target = document.querySelector("unitDisc"+this.dataset.qty);
     //     target.innerHTML = newValue;
         
-    //     var newCost = ((100-this.value)/100) * this.dataset.qty * document.getElementById("unitCost").value;
-    //     var target = document.getElementById("cost"+this.dataset.qty);
+    //     var newCost = ((100-this.value)/100) * this.dataset.qty * document.querySelector("unitCost").value;
+    //     var target = document.querySelector("cost"+this.dataset.qty);
     //     target.innerHTML = "£"+decimal2(newCost);
     // }
 
